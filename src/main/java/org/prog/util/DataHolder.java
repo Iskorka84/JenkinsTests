b35bb797af63cc85d6e988717fef8a03ce28e09f
operchuk <liliya.iskorka@gmail.com>
2023-02-22 01:41:42 +0200

    Initial commit

---
+++ src/main/java/org/prog/util/DataHolder.java
@@ -0,0 +1,31 @@
+package org.prog.util;
+
+import org.junit.Assert;
+
+import java.util.HashMap;
+
+public class DataHolder {
+
+  private final static DataHolder instance = new DataHolder();
+
+  private final HashMap<String, Object> holder;
+
+  private DataHolder() {
+    holder = new HashMap<>();
+  }
+
+  public static DataHolder getInstance() {
+    return instance;
+  }
+
+  public void put(String key, Object value) {
+    holder.put(key, value);
+  }
+
+  public Object get(String key) {
+    if (!holder.containsKey(key)) {
+      Assert.fail("Key not found " + key);
+    }
+    return holder.get(key);
+  }
+}
---
+++ src/test/java/org/prog/plugin/CucumberHooks.java
@@ -0,0 +1,17 @@
+package org.prog.plugin;
+
+import io.cucumber.plugin.EventListener;
+import io.cucumber.plugin.event.EventPublisher;
+import io.cucumber.plugin.event.TestRunFinished;
+
+public class CucumberHooks implements EventListener {
+
+    @Override
+    public void setEventPublisher(EventPublisher eventPublisher) {
+        eventPublisher.registerHandlerFor(TestRunFinished.class, this::tearDown);
+    }
+
+    private void tearDown(TestRunFinished event) {
+        System.out.println("All tests finished, shutting down...");
+    }
+}
